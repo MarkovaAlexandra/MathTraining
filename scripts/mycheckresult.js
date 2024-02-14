2a11afcdc2b129d1d9c9d493352699c43dfdbc83
Alexandra <alexandramarkova573@gmail.com>
2024-02-14 16:57:38 +0300

    валидация инпутов

--- scripts/mycheckresult.js
+++ scripts/mycheckresult.js
@@ -1,13 +1,15 @@
 function mycheck(result) {
-
-    let userChoice = userResult.value;
-    console.log(userChoice);
-    if (userChoice == result) {
-        rightWrong.innerHTML = 'Верно!';
-        statisticRight.innerHTML = Number(statisticRight.innerHTML) + 1;
-    }
+    if (userResult.value == '' || isNaN(Number(userResult.value))) alert('введите число');
     else {
-        rightWrong.innerHTML = ':(((';
-        statisticWrong.innerHTML = Number(statisticWrong.innerHTML) + 1;
+        let userChoice = userResult.value;
+        console.log(userChoice);
+        if (userChoice == result) {
+            rightWrong.innerHTML = 'Верно!';
+            statisticRight.innerHTML = Number(statisticRight.innerHTML) + 1;
+        }
+        else {
+            rightWrong.innerHTML = ':(((';
+            statisticWrong.innerHTML = Number(statisticWrong.innerHTML) + 1;
+        }
     }
 }
--- scripts/plusStolbik.js
+++ scripts/plusStolbik.js
@@ -7,6 +7,7 @@
 let first = document.querySelectorAll('.firstRazriad');
 let second = document.querySelectorAll('.secondRazriad');
 let userResult = document.querySelectorAll('.userInputStolbik');
+let overInCalc = document.querySelectorAll('.over-calculations');
 let checkBtn = document.querySelector('.check');
 let startBtn = document.querySelectorAll('.start');
 let statisticRight = document.querySelector('.right-value');
@@ -24,8 +25,9 @@ function start() {
     if (statisticRight.innerHTML == "") statisticRight.innerHTML = '0';
     if (statisticWrong.innerHTML == "") statisticWrong.innerHTML = '0';
     rightWrong.innerHTML = "";
-    userResult.value = "";
+    // userResult.value = "";
     userResult.forEach((item) => item.value = '');
+    overInCalc.forEach((item) => item.value = '');
     //рандомим слагаемые
     let max = 999;
     let min = 11;
@@ -36,10 +38,10 @@ function start() {
     let min2 = 11;
     let secondValue = Math.floor(Math.random() * (max2 - min2 + 1) + min2);
 
-    console.log(firstValue, "  ", secondValue);
+    // console.log(firstValue, "  ", secondValue);
     result = firstValue + secondValue;
-    console.log('1 = ', result);
-    console.log(first.length);
+    // console.log('1 = ', result);
+    // console.log(first.length);
     for (let i = first.length - 1; i >= 0; i--) {
         first[i].innerHTML = firstValue % 10;
         firstValue = Math.floor(firstValue / 10);
@@ -54,19 +56,55 @@ function start() {
 
 
 function mycheck(result) {
-    console.log('пишу из чека, result = ', result);
-    console.log(userResult[0].value);
-    let userChoice = userResult[0].value;
-    for (let i = 1; i < userResult.length; i++) {
-        userChoice = userChoice + userResult[i].value;
+    //если не прошли валидацию - return
+    if (!validate()) return;
+    //если все хорошо, то собираем результат
+    else {
+        let userChoice = userResult[0].value;
+        for (let i = 1; i < userResult.length; i++) {
+            userChoice = userChoice + userResult[i].value;
+        }
+        // console.log(userChoice);
+        if (userChoice == result) {
+            rightWrong.innerHTML = 'Верно!';
+            statisticRight.innerHTML = Number(statisticRight.innerHTML) + 1;
+        }
+        else {
+            rightWrong.innerHTML = ':(((';
+            statisticWrong.innerHTML = Number(statisticWrong.innerHTML) + 1;
+        }
     }
-    if (userChoice == result) {
-        rightWrong.innerHTML = 'Верно!';
-        statisticRight.innerHTML = Number(statisticRight.innerHTML) + 1;
+}
+
+
+function validate() {
+    console.log(userResult[0].value);
+    //если последней цифры нет - сразу разворачиваем
+    if (userResult[userResult.length - 1].value == '') {
+        alert('введите ответ');
+        return false;
     }
     else {
-        rightWrong.innerHTML = ':(((';
-        statisticWrong.innerHTML = Number(statisticWrong.innerHTML) + 1;
-    }
+        for (let i = 1; i < userResult.length; i++) {
 
+            //поверка если есть предыдущее значение
+            if (userResult[i - 1].value != '') {
+                //а текущее изнан или пустое или больше 9'
+                if ((isNaN(parseInt(userResult[i].value)) || userResult[i].value == '' || (parseInt(userResult[i].value) > 9))) {
+                    console.log('есть предыдущее значение, текущее изнан или пустое или больше 9');
+                    alert('введите корректные данные');
+                    return false;
+                }
+            }
+            //если предыдущего значения нет, 
+            else if (userResult[i - 1].value == "") {
+            }
+            //а текущее изНан,но не пустое или больше 9
+            if ((isNaN(parseInt(userResult[i].value)) & userResult[i].value != "") || parseInt(userResult[i].value) > 9) {
+                console.log('предыдущего значения нет, а текущее изнан, но не пустое, или больше 9');
+                alert('введите корректные даные');
+                return false;
+            }
+        }
+    } return true;
 }
